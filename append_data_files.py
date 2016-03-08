3073ea2531e90ca2b7e11891146370db63906035
Kyle Frankovich <kylefrankovich@gmail.com>
2016-03-07 17:16:39 -0800

    updating function

--- append_data_files.py
+++ append_data_files.py
@@ -1,17 +1,15 @@
 __author__ = 'kfranko'
 
 def append_files(master_file_name_path, list_of_files):
-    with open(master_file_name_path, 'w') as output:
+    with open(master_file_name_path, 'a') as output: # append to first file (or master file of subject if simply adding new subjects)
         for fname in list_of_files:
-            if fname == list_of_files[0]: # only open entire file (including header) if it's the first file
-                with open(fname) as infile:
-                    for line in infile:
-                        output.write(line)
-            else:
-                with open(fname) as infile:
-                    next(infile) # skip header for all non-first files
-                    for line in infile:
-                        output.write(line)
+            with open(fname) as infile:
+                next(infile, None) # skip header for all non-first files
+                for line in infile:
+                    output.write(line)
 
+
+for fname in list_of_files:
+    print fname
 
 append_files(master_file_name_path, list_of_files)
