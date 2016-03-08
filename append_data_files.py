c7b409e55022adff24265435c6422ce19addeffc
Kyle Frankovich <kylefrankovich@gmail.com>
2016-03-07 16:28:55 -0800

    fixing up script...

--- append_data_files.py
+++ append_data_files.py
@@ -1,8 +1,17 @@
 __author__ = 'kfranko'
 
-def append_files(output_path, list_of_files, out_filename):
-    with open(output_path, 'w') as out_filename:
-    for fname in list_of_files:
-        with open(fname) as infile:
-            for line in infile:
-                outfile.write(line)
+def append_files(master_file_name_path, list_of_files):
+    with open(master_file_name_path, 'w') as output:
+        for fname in list_of_files:
+            if fname == list_of_files[0]: # only open entire file (including header) if it's the first file
+                with open(fname) as infile:
+                    for line in infile:
+                        output.write(line)
+            else:
+                with open(fname) as infile:
+                    next(infile) # skip header for all non-first files
+                    for line in infile:
+                        output.write(line)
+
+
+append_files(master_file_name_path, list_of_files)
