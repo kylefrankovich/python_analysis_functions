7e5bc2adfdf167b359544315fbdefa617061146e
Kyle Frankovich <kylefrankovich@gmail.com>
2016-03-08 17:00:14 -0800

    function complete
    works well, but might want to add some features going forward

--- append_data_files.py
+++ append_data_files.py
@@ -1,23 +1,17 @@
 __author__ = 'kfranko'
 
+
 def append_files(master_file_name_path, list_of_files, num_lines_per_sub):
-    with open(master_file_name_path, 'a') as output: # append to first file (or master file of subject if simply adding new subjects)
+    with open(master_file_name_path, 'a') as output:  # append to first file (or master file of subject
+        # if simply adding new subjects)
         for fname in list_of_files:
             with open(fname) as infile:
                 # print 'number of lines:', sum(1 for line in infile), '\n'
-                # if sum(1 for line in infile) == num_lines_per_sub:
-                    # print 'processing subject file:', fname, '\n'
-                next(infile, None) # skip header for all non-first files
-                for line in infile:
-                    output.write(line)
-                # else:
-                #     print 'subject has incorrect number of trials for this experiment:', fname
-
-
-for fname in list_of_files:
-    print fname
-
-# print 'number of lines:', sum(1 for line in infile), '\n'
-# if sum(1 for line in infile) == num_lines_per_sub:
-
-append_files(master_file_name_path, list_of_files, 257)
+                if sum(1 for line in infile) == num_lines_per_sub:
+                    print 'processing subject file:', fname, '\n'
+                    with open(fname) as infile:
+                        next(infile, None)  # skip header for all non-first files
+                        for line in infile:
+                            output.write(line)
+                else:
+                    print 'subject has incorrect number of trials for this experiment:', fname
