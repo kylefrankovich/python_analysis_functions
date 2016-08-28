8833ae854f7db57572636e570eae13cfa16f2606
Kyle Frankovich <kylefrankovich@Kyles-MacBook-Pro.local>
2016-08-27 17:14:14 -0700

    added files to process for LOTP_mk6

--- append_data_files.py
+++ append_data_files.py
@@ -19,7 +19,11 @@ def append_files(master_file_name_path, list_of_files, num_lines_per_sub):
                     print '***** subject has incorrect number of trials for this experiment:', fname
 
 
-file_path = '/Users/kfranko/Google Drive/Young_Hall_data_analysis/analysis_scripts/analysis_functions/python_analysis_functions/YH_LOTP_ladies_and_gentlemen_mk3.txt'
+file_path = '/Users/kfranko/Google Drive/Young_Hall_data_analysis' \
+            '/analysis_scripts/analysis_functions/python_analysis_functions' \
+            '/YH_LOTP_mk6_master_data.txt'
+
+file_path = '/Users/kylefrankovich/Google Drive/Young_Hall_data_analysis/analysis_scripts/analysis_functions/python_analysis_functions/YH_LOTP_mk6_master_data.txt'
 
 file_list_mk2 = ['3_LOTP_ladies_and_gentlemen.txt','4_LOTP_ladies_and_gentlemen.txt',
              '5_LOTP_ladies_and_gentlemen.txt','6_LOTP_ladies_and_gentlemen.txt',
@@ -34,12 +38,24 @@ file_list_mk2 = ['3_LOTP_ladies_and_gentlemen.txt','4_LOTP_ladies_and_gentlemen.
 
 file_list_mk3 = ['2_LOTP_ladies_and_gentlemen_mk3.txt']
 
+file_list_LOTP_mk6 = ['08_LOTP_mk6.txt','09_LOTP_mk6_fixed.txt',
+                      '08_LOTP_mk6.txt','10_LOTP_mk6.txt',
+                      '12_LOTP_mk6.txt','15_LOTP_mk6.txt',
+                      '16_LOTP_mk6.txt','17_LOTP_mk6.txt',
+                      '19_LOTP_mk6.txt','20_LOTP_mk6.txt',
+                      '21_LOTP_mk6.txt','22_LOTP_mk6.txt',
+                      '23_LOTP_mk6.txt','24_LOTP_mk6.txt',
+                      '26_LOTP_mk6.txt','1024_LOTP_mk6.txt',
+                      '1030_LOTP_mk6.txt',]
 
+len(file_list_LOTP_mk6)
 
 import os
 
 os.getcwd()
 
-append_files(file_path,file_list_mk3, 257)
+os.listdir(os.getcwd())
+
+append_files(file_path,file_list_LOTP_mk6, 257)
 
 print file_list
